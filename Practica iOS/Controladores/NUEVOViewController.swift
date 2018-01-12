1f0978e0f58e260a19459abebe3f67fc05c93ded
macOSSierra <macossierra@Mac-de-macOSSierra.local>
2018-01-12 16:48:58 +0100

    Prueba commit desde xCode

---
+++ Practica iOS/Controladores/NUEVOViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NUEVOViewController.swift
+//  Practica iOS
+//
+//  Created by macOSSierra on 12/1/18.
+//  Copyright © 2018 ucam. All rights reserved.
+//
+
+import UIKit
+
+class NUEVOViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
